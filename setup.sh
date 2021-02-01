386e3e958f3e069b1dbf0e6f68aefb04a36b1239
Thor <thorck@protonmail.com>
2021-02-01 14:29:20 +0000

    quickfix, cross contracts not available

--- setup.sh
+++ setup.sh
@@ -4,11 +4,9 @@
 sed -i "s/dummy/$1/g" Cargo.toml
 sed -i "s/dummy/$1/g" build.sh
 sed -i "s/dummy/$1/g" deploy.sh
-sed -i "s/dummy/$1/g" tests/general.rs
 
 target=`echo $1 | tr [A-Z] [a-z] | sed -e 's/^./\U&/g; s/ ./\U&/g'`
 sed -i "s/Dummy/$target/g" src/lib.rs
-sed -i "s/Dummy/$target/g" tests/general.rs
 
 mv README.md instructions.md
 echo "# $1" >> README.md
